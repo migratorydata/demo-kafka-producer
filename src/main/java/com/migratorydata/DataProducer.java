55d4c9e629384b97a51205c04b3921383bbba97a
laurentiu <laurentiu.luca@migratorydata.com>
2023-11-09 16:35:31 +0200

    refactoring.

--- src/main/java/com/migratorydata/DataProducer.java
+++ src/main/java/com/migratorydata/DataProducer.java
@@ -1,18 +1,16 @@
 package com.migratorydata;
 
+import com.migratorydata.client.MigratoryDataKafkaUtils;
+import com.migratorydata.client.MigratoryDataMessage;
 import com.thedeanda.lorem.Lorem;
 import com.thedeanda.lorem.LoremIpsum;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
-import java.nio.ByteBuffer;
-import java.util.Arrays;
-import java.util.Base64;
 import java.util.Properties;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.zip.Deflater;
 
 public class DataProducer {
 
@@ -33,21 +31,16 @@ public class DataProducer {
             @Override
             public void run() {
                 // generate random text
-                String text = lorem.getWords(30, 60);
-
-                // compress data and encode as base64
-                byte[] compressedText = compressAndEncode(text.getBytes());
-
-                System.out.println("Data compressed:" + new String(compressedText));
+                String text = lorem.getWords(1024, 2048);
 
                 // record topic and key points to MigratoryData subject /live/data
-                ProducerRecord<String, byte[]> record = new ProducerRecord<>("live",0, "data", compressedText);
+                ProducerRecord<String, byte[]> record = MigratoryDataKafkaUtils.createRecord("live", "data", text.getBytes(), true, MigratoryDataMessage.QoS.STANDARD, true);
 
-                // add necessary metadata to the record
-                addMessageMetaData(record, true, QoS.STANDARD, true);
+                System.out.println("Before compression size:" + (text.getBytes().length));
+                System.out.println("After compression size:" + (record.value().length));
+                System.out.println("Compression ratio:" + text.getBytes().length / (float)record.value().length);
 
                 producer.send(record);
-
             }
         }, 1000, 5000, TimeUnit.MILLISECONDS);
     }
@@ -55,34 +48,5 @@ public class DataProducer {
     public void stop() {
         producer.close();
         liveExecutor.shutdown();
-    }
-
-    public static void addMessageMetaData(ProducerRecord<String, byte[]> record, boolean compression, QoS qoS, boolean retained) {
-        record.headers().add("qos", new byte[]{(byte) qoS.code()});
-
-        record.headers().add("compression", new byte[] {(byte)(compression ? 1:0) });
-
-        record.headers().add("retained", new byte[] {(byte)(retained ? 1:0) });
-
-        record.headers().add("pushVersion", "6.0.0".getBytes());
-    }
-
-    public byte[] compressAndEncode(byte[] content) {
-        Deflater compressor = new Deflater(Deflater.DEFAULT_COMPRESSION, true);
-        compressor.setInput(content);
-        compressor.finish();
-
-        ByteBuffer compressData = ByteBuffer.allocate(8192);
-        byte[] compressChunk = new byte[8192];
-        do {
-            int written = compressor.deflate(compressChunk);
-            compressData.put(compressChunk, 0, written);
-        } while (!compressor.finished());
-
-        compressor.end();
-
-        compressData.flip();
-
-        return Base64.getEncoder().encode(Arrays.copyOf(compressData.array(), compressData.limit() - compressData.position()));
     }
 }
